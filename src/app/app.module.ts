2e9f4213df73e120faa14c38d5b98aa551fefad4
Vo Phong <phongwind21092001@gmail.com>
2023-09-18 16:00:29 +0700

    feat(ad): create book card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    HeaderComponent
+    HeaderComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule,
