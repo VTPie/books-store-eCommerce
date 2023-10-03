3fe320376aeb2a768f4036b4b286c0bda1ce2e19
Vo Phong <phongwind21092001@gmail.com>
2023-10-03 14:05:25 +0700

    feat(ad): add logout feat (account-page)

--- src/app/account-page/account-page.component.ts
+++ src/app/account-page/account-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare var window: any
 
 @Component({
   selector: 'app-account-page',
   templateUrl: './account-page.component.html',
   styleUrls: ['./account-page.component.scss']
 })
-export class AccountPageComponent {
+export class AccountPageComponent implements OnInit {
+  constructor(private router: Router) { }
 
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['home']);
+    this.formModal.hide()
+  }
+
+  //Show confirm logout modal
+  formModal: any
+  ngOnInit(): void {
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById("modal-confirm-logout")
+    )
+  }
+  openModal() {
+    this.formModal.show()
+  }
 }
