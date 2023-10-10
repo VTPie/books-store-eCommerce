0f981b94a229f5cb1457713390e94ddc9d7b5f96
Vo Phong <phongwind21092001@gmail.com>
2023-10-10 10:31:53 +0700

    feat(ad): validate payment-form

--- src/app/payment-page/payment-page.component.ts
+++ src/app/payment-page/payment-page.component.ts
@@ -1,28 +1,62 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CartService } from '../cart.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment-page',
   templateUrl: './payment-page.component.html',
   styleUrls: ['./payment-page.component.scss']
 })
-export class PaymentPageComponent {
-
+export class PaymentPageComponent implements OnInit {
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private toastr: ToastrService
   ) { }
+  ngOnInit(): void {
+    //FormControl
+    this.infoForm = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      phone: new FormControl('', [
+        Validators.required,
+        Validators.min(10000000),
+        Validators.max(99999999),
+      ]),
+      address: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10)
+      ]),
+    });
+  }
 
-  infoForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    address: new FormControl(''),
-  });
+  //FormControl
+  infoForm!: FormGroup
+  get name() { return this.infoForm.get('name')!; }
+  get email() { return this.infoForm.get('email')!; }
+  get phone() { return this.infoForm.get('phone')!; }
+  get address() { return this.infoForm.get('address')!; }
+
 
   //Get property from service CART
   cartList = this.cartService.getItems();
   cartListQtt = this.cartService.getQtt();
   cartListPrice = this.cartService.getPrice();
   totalAmount = this.cartService.getTotalPrice()
+
+  //Submit form
+  checkout() {
+    if (!this.infoForm.value.name || !this.infoForm.value.name || !this.infoForm.value.name || !this.infoForm.value.name) {
+      this.toastr.error('Input cannot be empty.', 'Sorry!');
+    }
+    else {
+      this.toastr.success('Payment success.', 'Congratulation!');
+    }
+  }
 }
